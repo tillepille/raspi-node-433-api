4e62d24cc6414e982026c455a205eca3145821e1
Tim Louis Schrumpf <TLS@Tims-iMac.fritz.box>
2018-01-09 14:44:29 +0100

    update for newer version of rpi-433(-tristate)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var rpi433    = require('rpi-433'),
       pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13)
       debounceDelay: 500          //Wait 500ms before reading another code
     }),
-    rfEmitter = rpi433.emitter({
+    rfEmitter = rpi433.emitterTriState({
       pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
       pulseLength: 350            //Send the code with a 350 pulse length
     });
