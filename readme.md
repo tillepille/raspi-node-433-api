3f2926041607c9585fc0ee044d2eb737f9e574c5
Tim Louis Schrumpf <TLS@Tims-iMac.fritz.box>
2018-01-10 11:12:34 +0100

    v1.0
    made a decent readme,
    improved the server in terms of logging and made clearer comments.

--- readme.md
+++ readme.md
@@ -1,21 +1,30 @@
-# NodeJs Wiring-Pi Webserve
+# Raspberry NodeJs 433MHz API
 
-based on xkonni/raspbery-remote idea with RCSwitch mechanisms.
-Also based on eroak/rpi-433 wich i forked and tweaked to myself, will make a pull request later...
+Inspired by the [xkonni/raspbery-remote](https://github.com/xkonni/raspberry-remote) idea with RCSwitch mechanisms. But unfortunately this has issues with Raspian Stretch...  
+Also based on [eroak/rpi-433](https://github.com/eroak/rpi-433) which I forked and tweaked to myself, will make a pull request later...
 
-Its supposed to switch 433MHz power outlets and intertechno switches by web. Mainly to use it with another NodeJS Instance of Homebridge.
-At least thats the plan...
+It's supposed to switch 433MHz ELRO style power outlets and intertechno switches by web. Mainly to use it with another NodeJS Instance of [Homebridge] (https://github.com/nfarina/homebridge).
+
 
 ## To-Do
-* Exception safety
-* make it work
+* more stability (exception safety,...)
 
-## Already Accomplished
-* generating codes for Intertechno and ELRO
-* untested minimal server frame
-* State of switches is working..
-* theoretically it is working but practically not.
+## How To use
+In order to run this node you need to sudo `sudo nodejs server.js`
 
-Example Call: `ip.add.ress:8080/?family=inter&familyCode=12&switchCode=11&onOff=1`
+The only dependency is on `rpi-433-tristate` at the moment, hopefully this goes back to the `rpi-433` standard version of eroak in a future version.
+On default this runs on Port 8080, modify the server.js if you want to use something else.
+To work properly you need to set the following attributes:
 
-**If you're a NodeJS Pro with too much time and knowledge of wiringPI please contact me, make a pull request,...**
+* family -> `elro` or `inter`. inter works for Intertechno devices with two wheels on the back. (2x 1-16 resp. A-F, 1-16).
+    elro should work for the cheapest 433MHz Outlets you can find. They have 10 dip switches you can use with binary numbering.
+* familyCode -> depends on family. First wheel or first 5 dip switches.See examples below
+* switchcode -> Second wheel or last 5 dip switches.
+* onOff -> **1 : switch on, 0 : switch off, 2 : get current status of switch**
+
+*Note: onOff=2 returns 1 if the switch is on or 0 if the switch is off. This can be used by homebridge to tell, if the switch is on or not, so the displaying is more reliable.*
+
+Example Call:           `ip.add.ress:8080/?family=inter&familyCode=12&switchCode=11&onOff=1`
+Another example Call:   `ip.add.ress:8080/?sys=elro&family=00100&switchCode=3&onOff=0`
+
+**If you're experiencing any problems or have improvement ideas, please open an issue, contact me, make a pull request,...**
--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 var http = require('http');
 var url = require('url');
 var rpi433    = require('rpi-433-tristate');
-
-rfSniffer = rpi433.sniffer({
-    pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13)
-    debounceDelay: 500          //Wait 500ms before reading another code
-});
+// setting up the mitter
 rfEmitter = rpi433.emitterTriState({
     pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
-    pulseLength: 300            //Send the code with a 350 pulse length
+    pulseLength: 300            //Send the code with a 300 pulse length
 });
 //Array for State of Switches
 var switchStateDict= {};
@@ -17,21 +13,21 @@ http.createServer(function (req, res) {
     var q = url.parse(req.url, true).query;
     var stat = mainController(q.sys,q.family, q.switchCode,q.onOff);
     //asked for status give success + status as plain text
-    if(stat == 0 && stat == 1){
+    if(stat == 0 || stat == 1){
         res.writeHead(200, {'Content-Type': 'text/html'});
         var txt = stat;
     //asked for switching, give if switch exists or nor
     }else if (stat == 200) {
         res.writeHead(stat,{'Content-Type': 'text/html'});
-        var txt = "Sucess!";
+        var txt = "switched: "+q.sys+" "+q.family+" "+q.switchCode;
     //any other gives an internal error
     }else if (stat == 404) {
         res.writeHead(stat,{'Content-Type': 'text/html'});
         var txt = "Couldn't find this Switch: "+q.sys+" "+q.family+" "+q.switchCode;
     }else {
-        console.log("Error in main controller.\n "+q.sys+" "+q.family+" "+q.switchCode+" status: "+stat);
+        console.log("Error!\n received: "+q.sys+" "+q.family+" "+q.switchCode+" status: "+stat);
         res.writeHead(502, {'Content-Type': 'text/html'});
-        var txt ="502 Internal Error";
+        var txt ="502 Internal Server Error!";
     }
     res.end(txt);
 }).listen(8080);
@@ -57,7 +53,7 @@ function mainController(sys, familyCode, switchCode, onOff){
             default:
             returnCode = 404;
         }
-        console.log("Sending Code " + code +"now...");
+        console.log("Sending Code " + code +" now...");
         rfEmitter.sendCode(code, function(error, stdout) {   //Send 1234
             if(!error) console.log(stdout); //Should display 1234
         });
@@ -68,7 +64,7 @@ function mainController(sys, familyCode, switchCode, onOff){
     }
 }
 
-//functions for current state
+// receive the current state of a switch
 function getState(familyCode, switchCode){
     var code = familyCode.toString()+switchCode.toString();
     var state = switchStateDict[code];
@@ -79,16 +75,17 @@ function getState(familyCode, switchCode){
         return -1;
     }
 }
-
+// update the current state of a switch
 function changeState(familyCode,switchCode,onOff){
     var code = familyCode.toString()+switchCode.toString();
     switchStateDict[code] = onOff;
     console.log(code+": "+switchStateDict[code]);
 }
 
-//Generate Code for Intertechno Switches for sending it via sendTriState()
+// Generate Code for Intertechno Switches for sending it via sendTriState()
 function generateIntertechnoCode(familyCode, switchCode, onOff){
-    var codes = ["0000", "F000", "0F00", "FF00", "00F0", "F0F0", "0FF0", "FFF0", "000F", "F00F", "0F0F", "FF0F", "00FF", "F0FF", "0FFF", "FFFF"];
+    var codes = ["0000", "F000", "0F00", "FF00", "00F0", "F0F0", "0FF0", "FFF0",
+                 "000F", "F00F", "0F0F", "FF0F", "00FF", "F0FF", "0FFF", "FFFF"];
     var finalCode = "";
     finalCode += codes[familyCode-1];
     finalCode += codes[switchCode-1];
@@ -101,7 +98,8 @@ function generateIntertechnoCode(familyCode, switchCode, onOff){
     console.log("generated inter Code: "+finalCode);
     return finalCode;
 }
-//Generate Code for ELRO Switches for sending it via sendTriState()
+
+// Generate Code for ELRO Switches for sending it via sendTriState()
 function generateElroCode(familyCode, switchCode, onOff){
     var finalCode = "";
     var familyCodeArray = familyCode.toString().split("");
@@ -123,7 +121,8 @@ function generateElroCode(familyCode, switchCode, onOff){
     }
     return finalCode;
 }
-//inverted == true 0->F, inverted == false 1
+
+// inverted == true 0->F, inverted == false 1
 function replaceOnes(string,inverted){
     var result = "";
     for (x in string){
@@ -143,42 +142,3 @@ function replaceOnes(string,inverted){
     }
     return result;
 }
-
-/*
-OBSOLETE
-
-//modelling the waveforms
-function sendTriState(code){
-    code = code.split("");
-    for(i in code){
-        switch (code[i]){
-            case "0":
-                transmit(1,3);
-                transmit(1,3);
-                break;
-            case "1":
-                transmit(3,1);
-                transmit(3,1);
-                break;
-            case "F":
-                transmit(1,3);
-                transmit(3,1);
-                break;
-        }
-    }
-    //sync bit
-    transmit(1,31);
-    console.log("Send TriState Signal: "+code);
-}
-//send actual Pulses of
-function transmit(highPulses,lowPulses){
-    for(var i= 0; i < highPulses;i++){
-        wpi.digitalWrite(wPin,1);
-        wpi.delayMicroseconds(delayTime);
-    }
-    for(var i = 0; i < lowPulses;i++){
-        wpi.digitalWrite(wPin,0);
-        wpi.delayMicroseconds(delayTime);
-    }
-}
-*/
